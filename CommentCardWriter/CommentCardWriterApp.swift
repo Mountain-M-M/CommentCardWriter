4ca9f1e2aaeca52c255551018605bcb2ccf8abd8
Matteo Mountain <mountain.m@etoncollege.org.uk>
2023-02-07 10:08:42 +0000

    Initial Commit

---
+++ CommentCardWriter/CommentCardWriterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommentCardWriterApp.swift
+//  CommentCardWriter
+//
+//  Created by Mountain, Matteo (IRG) on 07/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct CommentCardWriterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
